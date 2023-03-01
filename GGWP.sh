147c4ce834353d122ea4fe283cd160ece298eaa1
Larry Chiem <lartard2@gmail.com>
2023-03-01 15:21:05 -0800

    Update GGWP.sh

--- GGWP.sh
+++ GGWP.sh
@@ -3,7 +3,7 @@ mkdir /volumes/untitled/tmp
 cd /volumes/untitled/tmp
 sleep 5
 ECHO "Downloading MacOS Ventura Install Assistant.pkg"
-curl -OL --progress-bar https://swcdn.apple.com/content/downloads/45/03/012-93766-A_BKGVNEJYP3/ogzz9pg17x6lk7uvi0jgqjz843ncr7857a/InstallAssistant.pkg
+curl -OL --progress-bar https://swcdn.apple.com/content/downloads/26/15/032-48342-A_OG2YEE7OSX/8sd3qpy79cimb7cjiy47cytps0gm7m0z5l/InstallAssistant.pkg
 sleep 5
 ECHO "Installing Ventura Install Assistant @ /volumes/Untitled/Applications/"
 installer -verboseR -pkg /volumes/untitled/tmp/InstallAssistant.pkg -target "/volumes/untitled/"
